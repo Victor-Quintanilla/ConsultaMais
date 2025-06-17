57d17cfb35495e36ac99d369afbfd35003718b9b
Victor Quintanilla <victorgquintanilla@gmail.com>
2025-06-17 20:58:49 -0300

    Comunicação Front com Back
    Frontend consegue alimentar o banco de dados corretamente

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-const { login } = require('../controllers/authController');
+const { login, registrar } = require('../controllers/authController');
 
 router.post('/login', login);
 router.post('/registro', registrar);
